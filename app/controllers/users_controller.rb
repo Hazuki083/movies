b0917f7882d1319633b22078065b3d1fb2bd630e
Hazuki083 <ha882ki@gmail.com>
2021-10-29 03:00:59 +0000

    Add info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,11 @@ class UsersController < ApplicationController
   end
 
   def index
+    @user = current_user
+    @users = Users.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 end
